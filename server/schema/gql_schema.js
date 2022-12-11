61ceb734d6290c785254ab980f4d1bd6c77ceb6f
Niranga De Silva <nirangad@gmail.com>
2022-12-11 14:17:50 +0530

    Tank Mutations (Add tank)

--- server/schema/gql_schema.js
+++ server/schema/gql_schema.js
@@ -8,10 +8,10 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLSchema,
+  GraphQLList,
   GraphQLInt,
   GraphQLFloat,
   GraphQLBoolean,
-  GraphQLList,
 } = graphql;
 
 const types = {};
@@ -19,100 +19,6 @@ types.TankType = TankTypeInj(types);
 types.StatType = StatTypeInj(types);
 
 const { TankType, StatType } = types;
-
-// const TankType = new GraphQLObjectType({
-//   name: "Tank",
-//   fields: () => ({
-//     id: { type: GraphQLString },
-//     tank_id: { type: GraphQLString },
-//     name: { type: GraphQLString },
-//     short_name: { type: GraphQLString },
-//     slug: { type: GraphQLString },
-//     type: { type: GraphQLString },
-//     tier: { type: GraphQLInt },
-//     price: { type: GraphQLFloat },
-//     gold_price: { type: GraphQLFloat },
-//     not_in_shop: { type: GraphQLBoolean },
-//     nation: { type: GraphQLString },
-//     tags: { type: GraphQLString },
-//     stats: {
-//       type: new GraphQLList(StatType),
-//       async resolve(parent, args) {
-//         const stats = await StatTypeModel.find({ tank_id: parent.tank_id });
-//         return stats;
-//       },
-//     },
-//   }),
-// });
-
-// const StatType = new GraphQLObjectType({
-//   name: "Stat",
-//   fields: () => ({
-//     id: { type: GraphQLString },
-//     stat_id: { type: GraphQLInt },
-//     name: { type: GraphQLString },
-//     short_name: { type: GraphQLString },
-//     slug: { type: GraphQLString },
-//     type: { type: GraphQLString },
-//     tier: { type: GraphQLInt },
-//     price: { type: GraphQLFloat },
-//     gold_price: { type: GraphQLFloat },
-//     not_in_shop: { type: GraphQLBoolean },
-//     nation: { type: GraphQLString },
-//     tank_id: { type: GraphQLString },
-//     forward_speed: { type: GraphQLFloat },
-//     reverse_speed: { type: GraphQLFloat },
-//     hull_armor_front: { type: GraphQLFloat },
-//     hull_armor_side: { type: GraphQLFloat },
-//     hull_armor_rear: { type: GraphQLFloat },
-//     turret_armor_front: { type: GraphQLFloat },
-//     turret_armor_side: { type: GraphQLFloat },
-//     turret_armor_rear: { type: GraphQLFloat },
-//     chassis_rotation_speed: { type: GraphQLFloat },
-//     turret_rotation_speed: { type: GraphQLFloat },
-//     view_range: { type: GraphQLFloat },
-//     gun_name: { type: GraphQLString },
-//     elevation: { type: GraphQLFloat },
-//     depression: { type: GraphQLFloat },
-//     max_ammo: { type: GraphQLInt },
-//     reload_time: { type: GraphQLFloat },
-//     aim_time: { type: GraphQLFloat },
-//     dispersion: { type: GraphQLFloat },
-//     clip_size: { type: GraphQLInt },
-//     burst_size: { type: GraphQLFloat },
-//     shell_icon: { type: GraphQLString },
-//     shell_type: { type: GraphQLString },
-//     caliber: { type: GraphQLFloat },
-//     explosion_radius: { type: GraphQLFloat },
-//     damage: { type: GraphQLFloat },
-//     module_damage: { type: GraphQLFloat },
-//     speed: { type: GraphQLFloat },
-//     penetration: { type: GraphQLFloat },
-//     power: { type: GraphQLFloat },
-//     fire_chance: { type: GraphQLFloat },
-//     radio_range: { type: GraphQLFloat },
-//     health: { type: GraphQLInt },
-//     weight: { type: GraphQLFloat },
-//     potential_damage: { type: GraphQLFloat },
-//     dpm: { type: GraphQLFloat },
-//     pwr: { type: GraphQLFloat },
-//     stationary_camo: { type: GraphQLFloat },
-//     moving_camo: { type: GraphQLFloat },
-//     chassis: { type: GraphQLFloat },
-//     turret: { type: GraphQLFloat },
-//     gun: { type: GraphQLFloat },
-//     shell: { type: GraphQLFloat },
-//     engine: { type: GraphQLFloat },
-//     radio: { type: GraphQLFloat },
-//     tank: {
-//       type: TankType,
-//       async resolve(parent, _args) {
-//         const tank = await TankTypeModel.findOne({ tank_id: parent.tank_id });
-//         return tank;
-//       },
-//     },
-//   }),
-// });
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
@@ -127,7 +33,7 @@ const RootQuery = new GraphQLObjectType({
     },
     tanks: {
       type: new GraphQLList(TankType),
-      async resolve(_parent, args) {
+      async resolve(_parent) {
         const tanks = await TankTypeModel.find({});
         return tanks;
       },
@@ -142,9 +48,50 @@ const RootQuery = new GraphQLObjectType({
     },
     stats: {
       type: new GraphQLList(StatType),
-      async resolve(_parent, args) {
+      async resolve(_parent) {
         const stats = await StatTypeModel.find({});
         return stats;
+      },
+    },
+  },
+});
+const x = {
+  tank_id: "A01_T1_Cunningham",
+  name: "T1 Cunningham",
+  short_name: "T1",
+  slug: "t1",
+  type: "light",
+  tier: 1,
+  price: 0,
+  gold_price: 0,
+  not_in_shop: false,
+  nation: "usa",
+  tags: "lightTank,HD",
+};
+
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addTank: {
+      type: TankType,
+      args: {
+        tank_id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        short_name: { type: GraphQLString },
+        slug: { type: GraphQLString },
+        type: { type: GraphQLString },
+        tier: { type: GraphQLInt },
+        price: { type: GraphQLFloat },
+        gold_price: { type: GraphQLFloat },
+        not_in_shop: { type: GraphQLBoolean },
+        nation: { type: GraphQLString },
+        tags: { type: GraphQLString },
+      },
+      async resolve(_parent, args) {
+        const tank = new TankTypeModel(args);
+        tank.save();
+
+        return tank;
       },
     },
   },
@@ -152,4 +99,5 @@ const RootQuery = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: Mutation,
 });
